cb241e0e50d53f9e794cc3b21a92a1d563891afd
Ryan Goulden <percontation@gmail.com>
2014-03-12 23:34:56 -0400

    cocoa: fix native fullscreen
    This fixes a couple of issues with the Cocoa `--native-fs` mode, primarily:
    
     - A ghost titlebar at the top of the screen in full screen:
    
       This was caused by the window constraining code kicking in during
       fullscreen. Simply returning the unconstrained rect from the constraining
       method fixes the problem.
    
     - Incorrect behavior when using the titlebar buttons to enter/exit
       fullscreen, as opposed to the OSD button.
    
       This was caused by mpv's internal fullscreen state going out of sync with
       the NSWindow's one. This was the case because `toggleFullScreen:`
       completely bypassed the normal event flow that mpv expects.
    
    Signed-off-by: Ryan Goulden <percontation@gmail.com>
    
    Change style for mpv, simplify and refactor some of the constraining code.
    
    Signed-off-by: Stefano Pigozzi <stefano.pigozzi@gmail.com>

--- video/out/cocoa_common.m
+++ video/out/cocoa_common.m
@@ -304,7 +304,6 @@ static void create_window(struct vo *vo, uint32_t d_width, uint32_t d_height,
     if (opts->native_fs) {
         [s->window setCollectionBehavior:
             NSWindowCollectionBehaviorFullScreenPrimary];
-        [NSApp setPresentationOptions:NSFullScreenWindowMask];
     }
 }
 
@@ -503,8 +502,6 @@ static void vo_cocoa_fullscreen(struct vo *vo)
         [s->view setFullScreen:opts->fullscreen];
     }
 
-    [s->window didChangeFullScreenState];
-
     // Make the core aware of the view size change.
     resize_window(vo);
 }
@@ -630,9 +627,10 @@ int vo_cocoa_cgl_color_size(struct vo *vo)
 
 - (void)putCommand:(char*)cmd
 {
-    mp_cmd_t *cmdt = mp_input_parse_cmd(self.vout->input_ctx, bstr0(cmd), "");
+    char *cmd_ = ta_strdup(NULL, cmd);
+    mp_cmd_t *cmdt = mp_input_parse_cmd(self.vout->input_ctx, bstr0(cmd_), "");
     mp_input_queue_cmd(self.vout->input_ctx, cmdt);
-    ta_free(cmd);
+    ta_free(cmd_);
 }
 
 - (void)performAsyncResize:(NSSize)size {
