19ee5f6c31ae2ad9ff2333f40c63360e6e63a315
deuiore <deu@mailbox.org>
2017-11-21 11:27:22 +0100

    deu's build system.

---
+++ build.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+### Common functions
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+choice () {
+    echo ""
+    read -e -n 1 -p "$@ (y/n) [n]: " answer
+    [[ "${answer:0:1}" == [Yy] ]]
+}
+
+###
+
+pushd "$(dirname "$0")"
+
+echo "----------------------"
+echo "deu's mpv build system"
+echo "----------------------"
+
+pushd "external"
+BUILD_LIBS="$(pwd)/build_libs"
+
+if choice "Build ffmpeg?"; then
+    pushd "ffmpeg-mpv"
+    git pull
+    if choice "--- Reconfigure ffmpeg?"; then
+        make distclean
+        ./configure \
+            --prefix="${BUILD_LIBS}" \
+            --enable-static \
+            --disable-shared \
+            --enable-gpl \
+            --disable-debug \
+            --disable-doc \
+            --enable-gnutls
+    fi
+    make
+    make install
+    popd # ffmpeg-mpv
+fi
+
+if choice "Build libass?"; then
+    pushd "libass"
+    git pull
+    if choice "--- Reconfigure libass?"; then
+        make distclean
+        ./autogen.sh \
+            --prefix="${BUILD_LIBS}" \
+            --libdir="${BUILD_LIBS}/lib" \
+            --enable-static \
+            --disable-shared
+        ./configure \
+            --prefix="${BUILD_LIBS}" \
+            --libdir="${BUILD_LIBS}/lib" \
+            --enable-static \
+            --disable-shared
+    fi
+    make
+    make install
+    popd # libass
+fi
+
+if choice "Build fribidi?"; then
+    pushd "fribidi"
+    git pull
+    if choice "--- Reconfigure fribidi?"; then
+        make distclean
+        ./bootstrap
+        ./configure \
+            --prefix="${BUILD_LIBS}" \
+            --libdir="${BUILD_LIBS}/lib" \
+            --enable-static \
+            --disable-shared \
+            --without-glib
+    fi
+    make
+    make install
+    popd # fribidi
+fi
+
+popd # external
+
+if choice "Configure mpv?"; then
+    PKG_CONFIG_PATH="${BUILD_LIBS}/lib/pkgconfig" ./waf configure
+fi
+
+if choice "Build mpv?"; then
+    ./waf build
+fi
+
+popd # ORIGINAL DIR
